98bf71f983d391c02d695248102384ec883ca61f
iAmAyomide <ayomidekenny952@gmail.com>
2024-07-27 11:12:50 -0700

    home ready

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,32 @@
 import Image from "next/image";
+import Star from "/public/Star.svg"
+import HandStar from "/public/handstar.svg";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+    <main className="mobile px-8 md:px-12 mx-auto bg-mainBg h-[100dvh] relative overflow-hidden pt-[20rem]">
+      <div className="absolute bottom-0 right-10">
+        <Image alt=""  width={100} height={100} src={Star}/>
+      </div>
+      <div className="absolute -bottom-10 left-40">
+        <Image alt=""  width={100} height={100} src={Star}/>
+      </div>
+      <div className="absolute left-0">
+        <Image alt=""  width={100} height={100} src={Star}/>
+      </div>
+      <div className="absolute right-0">
+        <Image alt=""  width={100} height={100} src={Star}/>
+      </div>
+
+      <div className="absolute bottom-0 left-10">
+        <Image alt=""  width={100} height={100} src={Star}/>
+      </div>
+      <div className="absolute bottom-10 right-[15rem]">
+        <Image alt=""  width={100} height={100} src={Star}/>
+      </div>
+      <div className="absolute bottom-4 right-[10rem]">
+        <Image alt=""  width={100} height={100} src={Star}/>
+      </div>
     </main>
   );
 }
