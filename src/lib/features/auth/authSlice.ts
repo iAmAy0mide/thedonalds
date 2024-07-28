fec5f5ebd4cbb8267955bc476e5d14b9932b8ebc
iAmAyomide <ayomidekenny952@gmail.com>
2024-07-28 02:01:49 -0700

    update import

--- src/lib/features/auth/authSlice.ts
+++ src/lib/features/auth/authSlice.ts
@@ -1,15 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
-import type { PayloadAction } from '@reduxjs/toolkit'
+import type { PayloadAction } from '@reduxjs/toolkit';
 
 export interface AuthState {
     isLoggedIn: boolean;
+    isLoginModalOpen: boolean;
 }
 
 const initialState: AuthState = {
     isLoggedIn: localStorage.getItem("isLoggedIn") 
     // @ts-ignore
                 ? JSON.parse(localStorage.getItem("isLoggedIn")) 
-                : false
+                : false,
+                
+    isLoginModalOpen: true
 }
 
 export const AuthSlice = createSlice({
@@ -22,7 +25,13 @@ export const AuthSlice = createSlice({
         },
         signOut: (state) => {
             state.isLoggedIn = false;
-            localStorage.clear()
+            localStorage.clear();
+        },
+        setLoginModalStatus: (state, action) => {
+            state.isLoginModalOpen = action.payload;
         }
     }
-})
+});
+
+export const { setIsLoggedIn, signOut, setLoginModalStatus } = AuthSlice.actions;
+export default AuthSlice.reducer;
