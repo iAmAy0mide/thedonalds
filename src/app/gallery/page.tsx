6bf5b96999bf7fd895ded9858110b4ef82511b60
iAmAyomide <ayomidekenny952@gmail.com>
2024-07-29 02:36:17 -0700

    comments off

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,9 +1,11 @@
 import Album from '@/components/Albums/Album';
+import CommentsModal from "@/components/comments/CommentsModal"
+import UploadAlbumPhotosModal from "@/components/uploadPhoto/UploadAlbumPhotosModal";
 import React from 'react'
 
 const Gallery = () => {
   return (
-    <main className=' mt-[50dvh] -z-20 mobile container mx-auto px-8 md:px-16'>
+    <main className='pt-[50dvh] -z-20 mobile container mx-auto px-8 md:px-16'>
       <div className="flex gap-4 flex-wrap">
         <Album />
         {/* <div className="mx-auto"> */}
@@ -13,6 +15,12 @@ const Gallery = () => {
         <Album />
         <Album />
         <Album />
+      </div>
+      {/* <div className="absolute left-0 top-[10dvh] z-[600]">  
+        <CommentsModal />
+      </div> */}
+      <div className="absolute left-0 top-[10dvh] z-[600]">  
+        <UploadAlbumPhotosModal />
       </div>
     </main>
   )
