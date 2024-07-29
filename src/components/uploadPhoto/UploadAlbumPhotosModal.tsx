ef97a7657f90d96bc30f8c0cf3b4f7afe553bad5
iAmAyomide <ayomidekenny952@gmail.com>
2024-07-29 09:42:49 -0700

    comp uplaod modal

--- src/components/uploadPhoto/UploadAlbumPhotosModal.tsx
+++ src/components/uploadPhoto/UploadAlbumPhotosModal.tsx
@@ -5,13 +5,15 @@ const UploadAlbumPhotosModal = () => {
     <div className={`w-[100dvw] px-4 pt-4 mx-auto relative h-[90dvh]`}>
         <div className="absolute inset-0 glassy-green  z-[5]"></div>
         <div className=" relative z-20 bg-greenBg  h-[85dvh] border-[4px] border-red rounded-xl px-4">
-          <div className="sm:max-w-[35rem]  w-full h-full flex flex-col justify-center items-center  border border-red  mx-auto ">
+          <div className="sm:max-w-[35rem]  w-full h-full flex justify-center items-center  mx-auto ">
 
-            <div className="flex flex-col sm:flex-r ow gap-0">
-
-              <div className="flex flex-wrap overflow-scr oll  border border-red max-w-full w-full ">
-                {/* <div className="w-[4rem] h-[4rem] bg-actionBg rounded-xl"></div> */}
-                <div className="flex flex-wrap gap-2 w-full overflow-y-scroll max-h-[8rem]">
+            <div className="flex flex-col basis-min-content sm:flex-r ow gap-0 items-center">
+              <div className="flex max-w-full  w-full h-auto">
+                {/* <div className="w-[5rem] h-[4rem] bg-actionBg rounded-xl mr-2"></div> */}
+                <div className="flex flex-wrap  gap-2 w-full">
+                  <div className="w-[4rem] h-[4rem] bg-actionBg rounded-xl"></div>
+                  <div className="w-[4rem] h-[4rem] bg-actionBg rounded-xl"></div>
+                  <div className="w-[4rem] h-[4rem] bg-actionBg rounded-xl"></div>
                   <div className="w-[4rem] h-[4rem] bg-actionBg rounded-xl"></div>
                   <div className="w-[4rem] h-[4rem] bg-actionBg rounded-xl"></div>
                   <div className="w-[4rem] h-[4rem] bg-actionBg rounded-xl"></div>
@@ -24,8 +26,8 @@ const UploadAlbumPhotosModal = () => {
                 </div>
               </div>
               <div className="flex flex-col sm:flex-row  mx-auto ">
-                <div className="rounded-xl flex justify-center items-center w-[18rem] h-[12rem] bg-[#7B 85E1] glassy-blue-gradient mb-2 mt-6 ">
-                    <p className="text-[#fff] shadow brightness-110  shadow-[#fff] glassy-blu bg-actionBg rounded-3xl px-10 py-1 text-[1.5rem] ">Upload Photo</p>
+                <div className="rounded-xl flex justify-center items-center w-[18rem] h-[12rem] bg-[#7B85E1]  mb-2 mt-4 sm:mr-6 ">
+                    <p className="text-[#fff] brightness-110 glassy-blu bg-[#1b3653] rounded-3xl px-10 py-1 text-[1.2rem] ">Upload Photo</p>
                 </div>
                 <div className="flex flex-col sm:mt-6 ">
                   <p className='text-[#fff] mb -1'>Album Name</p>
