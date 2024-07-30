79618bbfd23837e026f6c3452d0ebb9a64d6b74a
iAmAyomide <ayomidekenny952@gmail.com>
2024-07-30 00:33:25 -0700

    reuseable modal

---
+++ src/components/Modal.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Modal = ({ children }: Readonly <{
+    children: React.ReactNode
+}>) => {
+  return (
+    <div className={`w-[100dvw] px-4 pt-4 mx-auto relative h-[90dvh]`}>
+        <div className="absolute inset-0 glassy-green  z-[5]"></div>
+        <div className="absolute inset-0 glassy-green  z-[5]"></div>
+        {children}
+    </div>
+  )
+}
+
+export default Modal
