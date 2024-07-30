2cd5dc2a0d752f382a0649ba3bbc72509aba9f15
iAmAyomide <ayomidekenny952@gmail.com>
2024-07-30 03:16:44 -0700

    updated

--- src/components/comments/CommentsModal.tsx
+++ src/components/comments/CommentsModal.tsx
@@ -14,16 +14,17 @@ const CommentsModal: React.FC = () => {
     <Modal> 
       <button onClick={() => {
         dispatch(setCommentModalStatus(false));
-      }} title='Close' aria-label='Close comment modal button' className="absolute -top-[2.5%] rounded-md hover:brightness-90 px-3 py-1 text-[#fff] text-sm bg-actionBg left-0 z-30">Close</button>       
-      <div className=" relative z-20 bg-greenBg  h-[85dvh] border-[4px] border-red rounded-xl px-4">
+      }} title='Close' aria-label='Close comment modal button' className="close-modal-button">Close</button>
+      <div className=" relative z-20 bg-greenBg   h-[85dvh] border-[4px] border-red rounded-xl px-4">
           <div className="h-full mt- w-full  justify-center items-center gap -2 rounded-xl flex flex-col mx-auto">
-              <div className="bg-red w-full  h-[25%] flex gap-2 bg-re d">
-                <div className="">
-                  <Image width={100} height={100} src={Human} alt='' aria-label='' title='Photo Album Display' className='  w-[10rem]' />
+              <div className="bg-red w-full basis-min-content  h-[25%] flex gap-2 bg-re d ">
+                <div className="h-full">
+                  <Image width={100} height={100} src={Human} alt='' aria-label='' title='Photo Album Display' className='' />
                 </div>
-                <div className="">
-                  <Image width={100} height={100} className=' w-[10rem]' src={Human} alt='' aria-label='' title='Photo Album Display' />
+                <div className="h-[8rem]">
+                  <Image width={100} height={100} src={Human} alt='' aria-label='' title='Photo Album Display' className='' />
                 </div>
+  
               </div>
               <div className="bg-actionBg rounded-xl h-[50%] sm:max-w-[35rem] md:max-w-[38rem]  w-full mt-2">
                 <h1>hshdjhd sd</h1>
