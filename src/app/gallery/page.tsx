5bce0a60d37f538fc18f798b2589a92e54cb2cc5
iAmAyomide <ayomidekenny952@gmail.com>
2024-07-30 01:37:33 -0700

    comentmodal cond

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,9 +1,14 @@
+"use client";
 import Album from '@/components/Albums/Album';
 import CommentsModal from "@/components/comments/CommentsModal"
 import UploadAlbumPhotosModal from "@/components/uploadPhoto/UploadAlbumPhotosModal";
-import React from 'react'
+import { RootState } from '@/lib/store';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Gallery = () => {
+  const isCommentModalOpen = useSelector((state: RootState) => (state.modal.isCommentModalOpen));
+  const isUploadAlbumPhotoModalOpen = useSelector((state: RootState) => (state.modal.isUploadPhotoModalOpen));
+
   return (
     <main className='pt-[50dvh] -z-20 mobile container mx-auto px-8 md:px-16'>
       <div className="flex gap-4 flex-wrap">
@@ -16,7 +21,7 @@ const Gallery = () => {
         <Album />
         <Album />
       </div>
-      <div className="absolute left-0 top-[10dvh] z-[600]">  
+      <div className={`${!isCommentModalOpen && "hidden"} absolute left-0 top-[10dvh] z-[600]`}>  
         <CommentsModal />
       </div>
       <div className="absolute left-0 top-[10dvh] z-[600]">  
