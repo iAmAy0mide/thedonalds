bb3435fe9cd9e9a8dee16c5f7acd748580f44a43
iAmAyomide <ayomidekenny952@gmail.com>
2024-08-01 00:34:17 -0700

    change to fixed

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -4,11 +4,15 @@ const Modal = ({ children }: Readonly <{
     children: React.ReactNode
 }>) => {
   return (
-    <div className={`w-[100dvw] px-4 pt-4 mx-auto relative h-[90dvh]`}>
-        <div className="absolute inset-0 glassy-green  z-[5]"></div>
+    <div className={`min-w-[100dvw] fixed -translate-x-[50%] left-[50%] px-4 pt-4 mx-auto glassy-green  h-[90dvh]`}>
         {children}
     </div>
   )
 }
 
 export default Modal
+
+    // <div className={`w-[100dvw] px-4 pt-4 mx-auto relative h-[90dvh]`}>
+    //     <div className="absolute inset-0 glassy-green  z-[5]"></div>
+    //     {children}
+    // </div>
