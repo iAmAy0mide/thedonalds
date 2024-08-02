3bc2ed660032f75fdfd07696b2f9cbad449ae2f3
iAmAyomide <ayomidekenny952@gmail.com>
2024-08-02 12:58:33 -0700

    ups

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -23,7 +23,7 @@ const Gallery = () => {
       <div className={`${!isCommentModalOpen && "hidden"} absolute left-0 top-[10dvh] z-[600]`}>  
         <CommentsModal />
       </div>
-      <div className={`${!isUploadAlbumPhotoModalOpen && "hidden"} absolute left-0 top-[10dvh] z-[600]`}>  
+      <div className={`${!isUploadAlbumPhotoModalOpen && "hid den"} absolute left-0 top-[10dvh] z-[600]`}>  
         <UploadAlbumPhotosModal />
       </div>
     </main> 
