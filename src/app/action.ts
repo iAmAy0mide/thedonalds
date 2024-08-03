a1626fa270133f8fc400ec0f4973a5610e6366c2
iAmAyomide <ayomidekenny952@gmail.com>
2024-08-02 20:16:47 -0700

    server funcs

---
+++ src/app/action.ts
@@ -0,0 +1,13 @@
+"use server";
+import Album from "./models/album";
+import Comment from "./models/comments";
+import User from "./models/users";
+
+export const addAlbum = async () => {
+
+}
+
+export const addComment = async () => {
+
+}
+
