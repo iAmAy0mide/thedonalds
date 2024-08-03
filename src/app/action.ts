1a1d39d08285542dad395475feb222a35f96464c
iAmAyomide <ayomidekenny952@gmail.com>
2024-08-03 14:00:31 -0700

    setup comment action

--- src/app/action.ts
+++ src/app/action.ts
@@ -1,13 +1,21 @@
 "use server";
+import Comment from "@/app/models/comments";
+import dbConnect from "@/lib/db/dbConnect";
 import Album from "./models/album";
-import Comment from "./models/comments";
 import User from "./models/users";
 
-export const addAlbum = async (albumData: albumData) => {
+// export const addAlbum = async (formData: FormData) => {
     
-}
+// }
 
-export const addComment = async () => {
+export const addComment = async (formData: FormData) => {
+    await dbConnect();
+    
+    const comment = formData.get("comment");
+    console.log(comment);
+    
+    const newComment = new Comment({ comment });
 
+    await newComment.save(); 
 }
 
