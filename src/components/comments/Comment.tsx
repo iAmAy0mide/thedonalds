1bb6f2c705a2c8fb0068d2c57c791bc31c972c29
iAmAyomide <ayomidekenny952@gmail.com>
2024-08-03 11:51:46 -0700

    comment componet reueable

---
+++ src/components/comments/Comment.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+
+
+
+const Comment: React.FC<IComment> = ({ profilePic, comment}) => {
+  return (
+    <div className="rounded-lg relative self-start  mx-auto w-full sm:w-[70%] p-1 bg-deepBlue">
+        <div className=" h-[4rem] w-[4rem] rounded-lg float-left mr-2">
+        <Image width={100} height={100} src={profilePic} alt='' aria-label='' title='Photo Album Display' className='w-full rounded-lg float-left' />
+        </div>
+        <p className="comment-text">{comment}</p>
+    </div>
+  )
+}
+
+export default Comment;
