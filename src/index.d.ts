384fe5dc11985510b99a184f88c6f3fd83306c02
iAmAyomide <ayomidekenny952@gmail.com>
2024-08-03 04:05:40 -0700

    update types

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,3 +2,8 @@ declare interface IUploadedImage {
     name: string;
     url: string;
   }
+
+declare interface IUProp {
+    setAlbumPhotos: Dispatch<SetStateAction<IUploadedImage[]>>,
+    albumPhotos: IUploadedImage[]
+  }
