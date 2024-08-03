ae40334ddd413ffeb15e19c8e84bb63108b421e4
iAmAyomide <ayomidekenny952@gmail.com>
2024-08-03 14:01:00 -0700

    upadted form

--- src/components/comments/CommentForm.tsx
+++ src/components/comments/CommentForm.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import { addComment } from '@/app/action';
+
 
 const CommentForm = () => {
+
   return (
-    <form className="comment-container-form">
-        <textarea name="comment" id="comment" className='comment-form-input' />
+    <form action={addComment} className="comment-container-form">
+        <textarea name="comment" required id="comment" className='comment-form-input' />
         <div className="absolute border-red border-[2px] rounded-lg inset-1 -z-5"></div>
         <button className='comment-form-button'>Comment</button>
     </form>  
   )
 }
 
-export default CommentForm
+export default CommentForm;
