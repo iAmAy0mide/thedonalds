46942439361658b7e340879e7a851398e3155c63
iAmAyomide <ayomidekenny952@gmail.com>
2024-08-03 08:37:40 -0700

    updaye acttion

--- src/app/action.ts
+++ src/app/action.ts
@@ -3,8 +3,8 @@ import Album from "./models/album";
 import Comment from "./models/comments";
 import User from "./models/users";
 
-export const addAlbum = async () => {
-
+export const addAlbum = async (albumData: albumData) => {
+    
 }
 
 export const addComment = async () => {
