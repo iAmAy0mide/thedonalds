e9630827dc80e9247da4e9672c35d3af309698ec
iAmAyomide <ayomidekenny952@gmail.com>
2024-08-03 08:37:25 -0700

    updaete filter

--- src/components/uploadAlbumPhoto/UploadAlbumForm.tsx
+++ src/components/uploadAlbumPhoto/UploadAlbumForm.tsx
@@ -1,19 +1,51 @@
-import React, { Dispatch, SetStateAction } from 'react';
+"use client";
+import React, { Dispatch, FormEvent, SetStateAction, useState } from 'react';
 
-const UploadAlbumForm: React.FC<IUProp> = ({setAlbumPhotos}) => {
-  
+// @ts-ignore
+const UploadAlbumForm: any = ({setAlbumPhotos, albumPhotos}) => {
+  const [albumName, setAlbumName] = useState<string>("");
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+
+
+    const albumData = {
+      albumName,
+      album: albumPhotos
+        .filter((photo: IUploadedImage) => photo.name !== "" && photo.url !== "")
+        .map((photo: IUploadedImage) => ({ image: { url: photo.url } }))    
+    }
+
+    console.log(albumData.album);
+    try {
+      const res = await fetch("/api/album", {
+        method: "POST",
+        body: JSON.stringify(albumData),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+
+      setAlbumPhotos([{
+        name: "",
+        url: ""
+      }]);
+      setAlbumName("");      
+    } catch (error) {
+      console.log(error);
+      
+    }
+    
+  }
 
   return (
-    <form action={} className="flex flex-col sm:mt-6 ">
+    <form onSubmit={handleSubmit} className="flex flex-col sm:mt-6 ">
         <p className='text-[#fff] mb -1'>Album Name</p>
-        <input type="text" name="album-name" id="album-name" className='h-9 px-2 rounded-md outline-none border-0 ' />
-        <button className='upload-album-button'>Add Album</button>
+        <input type="text" value={albumName} onChange={(e) => setAlbumName(e.target.value)} required name="albumName" id="albumName" className='h-9 px-2 rounded-md outline-none border-0 ' />
+        <button className='upload-album-button'>Create Album</button>
     </form>
   )
 }
 
 export default UploadAlbumForm;
-
-interface IUProp {
-  setAlbumPhotos: Dispatch<SetStateAction<IUploadedImage[]>>
-}
