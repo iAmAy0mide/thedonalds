03383953e2a92982d6307bebcf52e06814b7f4a1
iAmAyomide <ayomidekenny952@gmail.com>
2024-08-04 02:13:29 -0700

    ups

--- src/components/comments/Comment.tsx
+++ src/components/comments/Comment.tsx
@@ -1,11 +1,9 @@
 import Image from "next/image";
 
 
-
-
-const Comment: React.FC<IComment> = ({ profilePic, comment}) => {
+const Comment: React.FC<IComment> = ({ profilePic, comment, key}) => {
   return (
-    <div className="rounded-lg relative self-start  mx-auto w-full sm:w-[70%] p-1 bg-deepBlue">
+    <div key={key} className="rounded-lg relative self-start  mx-auto w-full sm:w-[70%] p-1 bg-deepBlue">
         <div className=" h-[4rem] w-[4rem] rounded-lg float-left mr-2">
         <Image width={100} height={100} src={profilePic} alt='' aria-label='' title='Photo Album Display' className='w-full rounded-lg float-left' />
         </div>
@@ -13,5 +11,5 @@ const Comment: React.FC<IComment> = ({ profilePic, comment}) => {
     </div>
   )
 }
-
+ 
 export default Comment;
