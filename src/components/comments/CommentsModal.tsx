40268990d91e826db3cc307b0ccd559365f67392
iAmAyomide <ayomidekenny952@gmail.com>
2024-08-04 15:56:47 -0700

    added optional chaining to co

--- src/components/comments/CommentsModal.tsx
+++ src/components/comments/CommentsModal.tsx
@@ -9,6 +9,7 @@ import Comment from './Comment';
 import { getComments } from '@/app/action';
 import useComments from '@/hooks/useComments';
 import AlbumPhotoCarousel from '../Albums/AlbumPhotoCarousel';
+import Comments from './Comments';
 
 
 
@@ -57,19 +58,8 @@ const CommentsModal: React.FC = () => {
                 </div>  
               </div>
 
-              {/* <AlbumPhotoCarousel /> */}
-
-              <div className="bg-actionBg rounded-xl h-[50%] p-1 sm:max-w-[35rem] space-y-2 flex flex-col items-center  md:max-w-[38rem] overflow-auto  w-full mt-2">
+              {/* <div className="bg-actionBg rounded-xl h-[50%] p-1 sm:max-w-[35rem] space-y-2 flex flex-col items-center  md:max-w-[38rem] overflow-auto  w-full mt-2">
                 
-                {/* <div className="rounded-lg relative self-start  mx-auto w-full sm:w-[70%] p-1 bg-deepBlue">
-                  <div className=" h-[4rem] w-[4rem] rounded-lg float-left mr-2">
-                    <Image width={100} height={100} src={Human} alt='' aria-label='' title='Photo Album Display' className='w-full rounded-lg float-left' />
-                  </div>
-                  <p className="comment-text">How many times were you frustrated while looking
-                  out for a good collection of programming / algorithm provide well written, well thought and well algorithm provide well written, well thought and well algorithm provide well written, well thought and well.
-                  </p>
-                </div> */}
-
                 {
                   comments.map((c) => (
                     <>
@@ -78,7 +68,8 @@ const CommentsModal: React.FC = () => {
                   ))
                 }
                
-              </div>  
+              </div>   */}
+              <Comments />
               <CommentForm />
           </div>
       </div>  
@@ -87,55 +78,15 @@ const CommentsModal: React.FC = () => {
   )
 }
 
-// <div className={`w-[100dvw] px-4 pt-4 mx-auto relative h-[90dvh]`}>
-      {/* <div onClick={() => { */}
-        // dispatch(setCommentModalStatus(false));
-      // }} className="absolute"></div>
-      {/* <div className="absolute inset-0 glassy-green  z-[5]"></div> */}
-
 export default CommentsModal;
 
 export const dynamic = "force-dynamic"
 
- {/* <Swiper
-        effect={'coverflow'}
-        grabCursor={true}
-        centeredSlides={true}
-        slidesPerView={'auto'}
-        coverflowEffect={{
-          rotate: 50,
-          stretch: 0,
-          depth: 100,
-          modifier: 1,
-          slideShadows: true,
-        }}
-        pagination={true}
-        modules={[EffectCoverflow, Pagination]}
-        className="bg-red h-[20rem] swiper w-[30rem] "
-      >
-        <SwiperSlide className="swiper-slide">
-          <Image width={100} height={100} src={Human} alt='' aria-label='' title='Photo Album Display' className='w-full img' />
-        </SwiperSlide>
-        <SwiperSlide className="swiper-slide">
-          <Image width={100} height={100} src={Human} alt='' aria-label='' title='Photo Album Display' className='w-full img' />
-        </SwiperSlide>
-        <SwiperSlide className="swiper-slide">
-          <Image width={100} height={100} src={Human} alt='' aria-label='' title='Photo Album Display' className='w-full img' />
-        </SwiperSlide>
-        <SwiperSlide className="swiper-slide">
-          <Image width={100} height={100} src={Human} alt='' aria-label='' title='Photo Album Display' className='w-full img' />
-        </SwiperSlide>
-        <SwiperSlide className="swiper-slide">
-          <Image width={100} height={100} src={Human} alt='' aria-label='' title='Photo Album Display' className='w-full img' />
-        </SwiperSlide>
-        <SwiperSlide className="swiper-slide">
-          <Image width={100} height={100} src={Human} alt='' aria-label='' title='Photo Album Display' className='w-full' />
-        </SwiperSlide>
-        <SwiperSlide className="swiper-slide">
-          <Image width={100} height={100} src={Human} alt='' aria-label='' title='Photo Album Display' className='w-full' />
-        </SwiperSlide>
-        <SwiperSlide className="swiper-slide">
-          <Image width={100} height={100} src={Human} alt='' aria-label='' title='Photo Album Display' className='w-full' />
-        </SwiperSlide>
-      
-      </Swiper> */}
+ {/* <div className="rounded-lg relative self-start  mx-auto w-full sm:w-[70%] p-1 bg-deepBlue">
+                  <div className=" h-[4rem] w-[4rem] rounded-lg float-left mr-2">
+                    <Image width={100} height={100} src={Human} alt='' aria-label='' title='Photo Album Display' className='w-full rounded-lg float-left' />
+                  </div>
+                  <p className="comment-text">How many times were you frustrated while looking
+                  out for a good collection of programming / algorithm provide well written, well thought and well algorithm provide well written, well thought and well algorithm provide well written, well thought and well.
+                  </p>
+                </div> */}
