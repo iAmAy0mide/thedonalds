2071b5562890f8f64ab1e98042809cc5d6bd1af0
iAmAyomide <ayomidekenny952@gmail.com>
2024-08-04 05:23:41 -0700

    convert to client compo

--- src/components/comments/CommentForm.tsx
+++ src/components/comments/CommentForm.tsx
@@ -1,27 +1,62 @@
-import React from 'react'
+"use client";
+
+import {FormEvent, useState} from 'react';
 import { addComment } from '@/app/action';
 import CommentButton from './CommentButton';
 import useComments from '@/hooks/useComments';
 
 
 const CommentForm = () => {
-  const { comments, getAllComment } = useComments();
-  function h() {
-    setTimeout(() => {
+  const { getAllComment } = useComments();
+  const [comment, setComment] = useState<string>("");
 
-      // alert("Time Up");
-      
+  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    const res = await fetch("/api/comments", {
+      method: "POST",
+      body: JSON.stringify(comment),
+      headers: {
+        "Content-Type": "application/json",
+      }
+    });
+    
+    if (res.ok) {
+      setComment("");
       getAllComment();
-    }, 1000);
+    }
   }
   return (
-    <form onSubmit={h} action={addComment} className="comment-container-form">
-        <textarea name="comment" required id="comment" className='comment-form-input' />
+    <form onSubmit={handleSubmit} className="comment-container-form">
+        <textarea 
+          name="comment" 
+          value={comment} 
+          onChange={(e) => setComment(e.target.value)} required id="comment" className='comment-form-input' />
         <div className="absolute border-red border-[2px] rounded-lg inset-1 -z-5"></div>
-        {/* <CommentButton /> */}
         <button className='comment-form-button'>Comment</button>
     </form>  
   )
 }
 
 export default CommentForm;
+// const CommentForm = () => {
+//   const { comments, getAllComment } = useComments();
+//   function h() {
+//     setTimeout(() => {
+
+//       // alert("Time Up");
+      
+//       getAllComment();
+//     }, 1000);
+//   }
+//   return (
+//     <form onSubmit={h} action={addComment} className="comment-container-form">
+//         <textarea name="comment" required id="comment" className='comment-form-input' />
+//         <div className="absolute border-red border-[2px] rounded-lg inset-1 -z-5"></div>
+//         {/* <CommentButton /> */}
+//         <button className='comment-form-button'>Comment</button>
+//     </form>  
+//   )
+// }
+
+// export default CommentForm;
