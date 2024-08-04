8ebdc21cf25aee4f48bcd1f15964bbf43050c1a3
iAmAyomide <ayomidekenny952@gmail.com>
2024-08-04 02:11:03 -0700

    tried refecth on sub

--- src/components/comments/CommentForm.tsx
+++ src/components/comments/CommentForm.tsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import { addComment } from '@/app/action';
+import CommentButton from './CommentButton';
+import useComments from '@/hooks/useComments';
 
 
 const CommentForm = () => {
+  const { comments, getAllComment } = useComments();
+  function h() {
+    setTimeout(() => {
 
+      alert("Time Up");
+      
+      getAllComment();
+    }, 1000);
+  }
   return (
-    <form action={addComment} className="comment-container-form">
+    <form onSubmit={h} action={addComment} className="comment-container-form">
         <textarea name="comment" required id="comment" className='comment-form-input' />
         <div className="absolute border-red border-[2px] rounded-lg inset-1 -z-5"></div>
+        {/* <CommentButton /> */}
         <button className='comment-form-button'>Comment</button>
     </form>  
   )
