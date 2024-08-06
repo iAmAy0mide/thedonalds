d41857f0866762ed4842531047acd750b167e5db
iAmAyomide <ayomidekenny952@gmail.com>
2024-08-06 09:16:58 -0700

    updates

--- src/hooks/useComments.ts
+++ src/hooks/useComments.ts
@@ -1,30 +1,53 @@
+"use client";
 import { getComments } from "@/app/action";
 import { useEffect, useState } from "react";
 
-const useComments = () => {
+interface ICom {
+  comments: {
+    id: string;
+    comment: string;
+  }[]
+}
 
-    const [comments, setComments] = useState< any[] >([]);
+const useComments = (comments: { id: string; comment: string; }[] | (() => { id: string; comment: string; }[])) => {
+
+    const [famComments, setFamComments] = useState<{
+      id: string;
+      comment: string;
+    }[]>(comments);
     // const [comments, setComments] = useState< any[] | { error: any; } >([]);
     
     
-    async function getAllComments() {
-      const res = await fetch("/api/comments", { cache: "no-store" });
+    async function updateComments(pcomments: {
+      id: string;
+      comment: string;
+    }[]) {
+      // const res = await fetch("/api/comments", { cache: "no-store" });
 
       
-      const Comments = await res.json();
+      // const Comments = await res.json();
       // const Comments = await getComments();
       console.log("from Hook from hook from hook HHHHHHHHHHOOOOOKKKK");
-      
-      setComments(Comments);
 
+      
+      // setFamComments(() =>  [{ id: "", comment: ""}] );
+      setFamComments((prevComments) => {
+        console.log("updating comment...");
+        
+        return [...prevComments, ...pcomments]
+        // return [ ...pcomments ] 
+      });
+      // setFamComments((prevComments) =>  [...prevComments, ...pcomments] );
+      console.log(pcomments, "from upFUnc");
+      
     }
 
-    useEffect(() => {
+    // useEffect(() => {
 
-      getAllComments();
-    }, []); 
+    //   getAllComments();
+    // }, []); 
 
-    return { comments, getAllComments }
+    return { famComments, updateComments }
 }
 
 export default useComments;
