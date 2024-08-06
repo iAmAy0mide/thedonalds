2fe11c21bf34b8950955027ca72ad0fccc7486da
iAmAyomide <ayomidekenny952@gmail.com>
2024-08-05 23:16:08 -0700

    action updated

--- src/app/action.ts
+++ src/app/action.ts
@@ -3,43 +3,63 @@ import Comment from "@/app/models/comments";
 import dbConnect from "@/lib/db/dbConnect";
 import Album from "./models/album";
 import User from "./models/users";
+import { revalidatePath } from "next/cache";
+import { NextResponse } from "next/server";
 
-// export const addAlbum = async (formData: FormData) => {
-    
-// }
 
 export const addComment = async (formData: FormData) => {
+// export const addComment = async (previousState: any ,formData: FormData) => {
     try {
         await dbConnect();
     
         const comment = formData.get("comment");
         
         const newComment = new Comment({ comment });
-    
         await newComment.save(); 
-        formData.append("comment", "");
+
+        const savedComment = newComment.map((c: { _id: { toString: () => any; }; comment: any; }) => {
+            const id =  c._id.toString();
+            const comment = c.comment;
+            return (
+                {_id: id, comment}
+             )
+        })    
+        
+
+        revalidatePath("/gallery");
+        revalidatePath("/");
+        revalidatePath("*");
+        return savedComment;
+        // formData.append("comment", "");
     } catch (error: any) {
         return {error: error.message}
     }
    
 }
 
+interface IComment {
+    com: {_id: string, comment: string}[]
+}
 
 export async function getComments() {
-    try {
+    // try {
         await dbConnect();
 
-        const comments = await Comment.find({}, { __v: 0 }).sort({ createdAt: -1 });
-        console.log(comments);
+        const comments = await Comment.find({}, { __v: 0, createdAt: 0, updatedAt: 0 }).sort({ createdAt: -1 });
+        // console.log(comments);
 
-        // return comments.map(comment => comment.toObject()); 
-        return comments.map(comment => {
-            const commentObj = comment.toObject();
-            commentObj._id = commentObj._id.toString(); // Convert _id to string
-            return commentObj;
-        });
-    } catch (error: any) {
-        return {error: error.message}
-    }
+        const convertedComments = comments.map(c => {
+                const id =  c._id.toString();
+                const comment = c.comment;
+                return (
+                    {_id: id, comment}
+                );
+        })
+
+        return convertedComments;    
+        // return comments as {_id: string, comment: string}[];   
+    // } catch (error: any) {
+    //     return [{error: error.message}]
+    // }
     
 }
