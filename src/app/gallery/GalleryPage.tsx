68a51a0e6254e6f7deddea6c09a18425f23680f2
iAmAyomide <ayomidekenny952@gmail.com>
2024-08-06 09:50:57 -0700

    removed comments

--- src/app/gallery/GalleryPage.tsx
+++ src/app/gallery/GalleryPage.tsx
@@ -1,13 +1,9 @@
 "use client";
 import Album from '@/components/Albums/Album';
-import CommentsModal from "@/components/comments/CommentsModal";
 import UploadAlbumPhotosModal from "@/components/uploadAlbumPhoto/UploadAlbumPhotosModal";
 import { RootState } from '@/lib/store';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
-interface IGPProp {
-  CommentsModal:  () => JSX.Element;
-}
 
 const GalleryPage = ({ children }: Readonly<{
   children: React.ReactNode
@@ -37,40 +33,4 @@ const GalleryPage = ({ children }: Readonly<{
 }
 
 export default GalleryPage;
-// "use client";
-// import Album from '@/components/Albums/Album';
-// import CommentsModal from "@/components/comments/CommentsModal";
-// import UploadAlbumPhotosModal from "@/components/uploadAlbumPhoto/UploadAlbumPhotosModal";
-// import { RootState } from '@/lib/store';
-// import { useDispatch, useSelector } from 'react-redux';
 
-// const GalleryPage = ({
-//   children,
-// }: Readonly<{
-//   children: React.ReactNode;
-// }>) => {
-//   const isCommentModalOpen = useSelector((state: RootState) => (state.modal.isCommentModalOpen));
-//   const isUploadAlbumPhotoModalOpen = useSelector((state: RootState) => (state.modal.isUploadPhotoModalOpen));
-
-//   return (
-//     <main className='pt-[35dvh] lg:pt-[40dvh] rela tive overflow-hidden -z-20 min-h-screen'>
-//       `<div className="mobile container mx-auto px-8 md:px-16">
-//         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3  gap-2 max-w-[1440px]  w-full justify-center">
-//           <Album />
-//           <Album />
-//           <Album />
-//           <Album />
-//           <Album />
-//         </div>
-//       </div>`      
-//       <div className={`${!isCommentModalOpen && "hidden"} absolute left-0 top-[10dvh] z-[600]`}>  
-//         {children} 
-//       </div>
-//       <div className={`${!isUploadAlbumPhotoModalOpen && "hidden"} absolute left-0 top-[10dvh] z-[600]`}>  
-//         <UploadAlbumPhotosModal />
-//       </div>
-//     </main> 
-//   )
-// }
-
-// export default GalleryPage;
--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,17 +1,7 @@
-// "use client";
-import Album from '@/components/Albums/Album';
-import CommentModalWithChildren from '@/components/comments/CommentModalWithChildren';
 import CommentsModal from "@/components/comments/CommentsModal";
-import UploadAlbumPhotosModal from "@/components/uploadAlbumPhoto/UploadAlbumPhotosModal";
-import { RootState } from '@/lib/store';
-import { useDispatch, useSelector } from 'react-redux';
 import GalleryPage from './GalleryPage';
 
 const Gallery = () => {
-  async function g() {
-    "use server"
-    
-  }
 
   return (
     <>
@@ -24,37 +14,3 @@ const Gallery = () => {
 
 export default Gallery;
 
-// "use client";
-// import Album from '@/components/Albums/Album';
-// import CommentModalWithChildren from '@/components/comments/CommentModalWithChildren';
-// import CommentsModal from "@/components/comments/CommentsModal";
-// import UploadAlbumPhotosModal from "@/components/uploadAlbumPhoto/UploadAlbumPhotosModal";
-// import { RootState } from '@/lib/store';
-// import { useDispatch, useSelector } from 'react-redux';
-
-// const Gallery = () => {
-//   const isCommentModalOpen = useSelector((state: RootState) => (state.modal.isCommentModalOpen));
-//   const isUploadAlbumPhotoModalOpen = useSelector((state: RootState) => (state.modal.isUploadPhotoModalOpen));
-
-//   return (
-//     <main className='pt-[35dvh] lg:pt-[40dvh] rela tive overflow-hidden -z-20 min-h-screen'>
-//       `<div className="mobile container mx-auto px-8 md:px-16">
-//         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3  gap-2 max-w-[1440px]  w-full justify-center">
-//           <Album />
-//           <Album />
-//           <Album />
-//           <Album />
-//           <Album />
-//         </div>
-//       </div>`      
-//       <div className={`${!isCommentModalOpen && "hidden"} absolute left-0 top-[10dvh] z-[600]`}>  
-//         <CommentModalWithChildren />
-//       </div>
-//       <div className={`${!isUploadAlbumPhotoModalOpen && "hidden"} absolute left-0 top-[10dvh] z-[600]`}>  
-//         <UploadAlbumPhotosModal />
-//       </div>
-//     </main> 
-//   )
-// }
-
-// export default Gallery;
