805099f4e6d0fd476de6a1c737e47e024f782e2f
iAmAyomide <ayomidekenny952@gmail.com>
2024-08-06 09:16:48 -0700

    updates

---
+++ src/components/comments/CommentsFetch.tsx
@@ -0,0 +1,19 @@
+import Comments from "./Comments";
+
+const CommentsFetch = async () => {
+    // const comments = await getComments();
+    const URL = process.env.environment === "prod" ? 'https://thedonalds.vercel.app' : "http://localhost:3000";    
+
+    const res = await fetch(`${URL}/api/comments`);
+    const comments = await res.json();
+    // console.log(comments, "from commentPage"); 
+    
+
+  return (
+    <>   
+        <Comments comments={comments} />      
+    </>    
+  )
+}
+
+export default CommentsFetch;
