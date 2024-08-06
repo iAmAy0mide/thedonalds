6acbd6fa377655d38b99acd5e83244479ce2f763
iAmAyomide <ayomidekenny952@gmail.com>
2024-08-06 09:17:09 -0700

    updates

--- src/components/comments/CommentsModal.tsx
+++ src/components/comments/CommentsModal.tsx
@@ -1,28 +1,12 @@
-// "use client"
-import Image from 'next/image';
-import Human from "/public/human.svg";
-import { useDispatch } from 'react-redux';
-import { setCommentModalStatus } from '@/lib/features/modal/modalSlice';
 import Modal from '../Modal';
-import CommentForm from './CommentForm';
-import Comment from './Comment';
-import { getComments } from '@/app/action';
-import useComments from '@/hooks/useComments';
 import AlbumPhotoCarousel from '../Albums/AlbumPhotoCarousel';
-import Comments from './Comments';
 import CommentsModalCloseBtn from './CommentsModalCloseBtn';
 import CommentSection from './CommentSection';
-
+// import CommentSection fro./Commentsion';
+// import CommentSection from './CommentSection';
 
 
 const CommentsModal = () => {
-  // const dispatch = useDispatch();
-  
-  async function usde() {
-    "use server"
-  }
-
- 
   
   return (
     
@@ -40,20 +24,3 @@ const CommentsModal = () => {
 }
 
 export default CommentsModal;
-
-export const dynamic = "force-dynamic"
-
- {/* <div className="rounded-lg relative self-start  mx-auto w-full sm:w-[70%] p-1 bg-deepBlue">
-                  <div className=" h-[4rem] w-[4rem] rounded-lg float-left mr-2">
-                    <Image width={100} height={100} src={Human} alt='' aria-label='' title='Photo Album Display' className='w-full rounded-lg float-left' />
-                  </div>
-                  <p className="comment-text">How many times were you frustrated while looking
-                  out for a good collection of programming / algorithm provide well written, well thought and well algorithm provide well written, well thought and well algorithm provide well written, well thought and well.
-                  </p>
-                </div> */}
-
-
-
-                // { children }: Readonly<{
-                //   children: React.ReactNode
-                // }>
