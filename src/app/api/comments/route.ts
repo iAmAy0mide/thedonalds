f44be01db8f592c3ba5f32d391e9c151696f60bf
iAmAyomide <ayomidekenny952@gmail.com>
2024-08-05 23:16:28 -0700

    updaS

--- src/app/api/comments/route.ts
+++ src/app/api/comments/route.ts
@@ -8,24 +8,49 @@ export async function GET(req: NextRequest, res: NextResponse) {
     try {
         await dbConnect();
 
-        const comments = await Comment.find({}, { __v: 0 }).sort({ createdAt: -1 });
+        const comments = await Comment.find({}, { __v: 0, createdAt: 0, updatedAt: 0 }).sort({ createdAt: -1 });
         // console.log(comments);
 
-        // return comments.map(comment => comment.toObject()); 
-        const sortedComment = comments.map(comment => {
-            const commentObj = comment.toObject();
-            commentObj._id = commentObj._id.toString(); // Convert _id to string
-            return commentObj;
-        });
+        const convertedComments = comments.map(c => {
+                const id =  c._id.toString();
+                const comment = c.comment;
+                return (
+                    {_id: id, comment}
+                );
+        })
+
+        // return convertedComments;    
 
         revalidatePath("/gallery");
 
-        return NextResponse.json(sortedComment)
+        return NextResponse.json(convertedComments)
     } catch (error: any) {
         return  NextResponse.json({error: error.message})
     }
     
 }
+// export async function GET(req: NextRequest, res: NextResponse) {
+//     try {
+//         await dbConnect();
+
+//         const comments = await Comment.find({}, { __v: 0 }).sort({ createdAt: -1 });
+//         // console.log(comments);
+
+//         // return comments.map(comment => comment.toObject()); 
+//         const sortedComment = comments.map(comment => {
+//             const commentObj = comment.toObject();
+//             commentObj._id = commentObj._id.toString(); // Convert _id to string
+//             return commentObj;
+//         });
+
+//         revalidatePath("/gallery");
+
+//         return NextResponse.json(sortedComment)
+//     } catch (error: any) {
+//         return  NextResponse.json({error: error.message})
+//     }
+    
+// }
 
 
 export async function POST(req: NextRequest, res: NextResponse) {
