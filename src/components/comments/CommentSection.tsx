9fe6f902b77832c24ec409867c1092cfdb46ca87
iAmAyomide <ayomidekenny952@gmail.com>
2024-08-05 23:12:19 -0700

    organized comments

---
+++ src/components/comments/CommentSection.tsx
@@ -0,0 +1,13 @@
+import Comments from './Comments';
+import CommentForm from './CommentForm';
+
+const CommentSection = () => {
+  return (
+    <>
+      <Comments />
+      <CommentForm />
+    </>
+  )
+}
+
+export default CommentSection;
