960152bcab0e57ab1b2dee191ce6e798d2efbb42
iAmAyomide <ayomidekenny952@gmail.com>
2024-08-07 00:57:12 -0700

    updated schema

--- src/app/models/album.ts
+++ src/app/models/album.ts
@@ -9,6 +9,7 @@ export interface IAlbum extends Document {
     albumName: string;
     album: TAlbum;
     deleted?: boolean;
+    comments: string[]
 }
 
 const AlbumSchema: mongoose.Schema = new Schema({
@@ -29,10 +30,19 @@ const AlbumSchema: mongoose.Schema = new Schema({
         }],
         required: true,
     },
+
+    
+    comments: {
+        type: [{
+            comment: String
+        }]
+    },
+
     deleted: {
         type: Boolean,
         default: false
     }
+
 }, { timestamps: true});
 
 const Album = mongoose.models.Album || mongoose.model<IAlbum>("Album", AlbumSchema);
