883f221e25b5a3f02fb48e45bdb11956ba3372c2
iAmAyomide <ayomidekenny952@gmail.com>
2024-08-07 11:16:44 -0700

    solved caching issue

--- src/components/comments/CommentsFetch.tsx
+++ src/components/comments/CommentsFetch.tsx
@@ -1,12 +1,13 @@
+import { getComments } from "@/app/action";
 import Comments from "./Comments";
 
 const CommentsFetch = async () => {
     // const comments = await getComments();
     const URL = process.env.environment === "prod" ? 'https://thedonalds.vercel.app' : "http://localhost:3000";    
 
-    const res = await fetch(`${URL}/api/comments`);
+    const res = await fetch(`${URL}/api/comments`, { cache: "no-cache"});
     const comments = await res.json();
-    // console.log(comments, "from commentPage"); 
+    console.log(comments, "from commentPage");  
     
 
   return (
