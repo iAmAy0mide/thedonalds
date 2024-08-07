8113881d24a312a04114fa3c66e4de4ccd23414c
iAmAyomide <ayomidekenny952@gmail.com>
2024-08-06 23:18:16 -0700

    update styles

--- src/components/Albums/Album.tsx
+++ src/components/Albums/Album.tsx
@@ -12,7 +12,7 @@ const Album: React.FC = () => {
     const dispatch = useDispatch();
 
     return (
-        <div className='relative border border-deepBlue text-[#fff]  flex flex-col  overflow-hidden h-[18rem]  rounded-xl' >
+        <div className='relative border bg-[#394c4c] border-deepBlue text-[#fff]  flex flex-col  overflow-hidden h-[18rem]  rounded-xl' >
             <div className="rounded-xl relative  h-[82%]" style={{ backgroundImage: `url(${Human.src})`, backgroundPosition: "center", backgroundSize: "cover" }}>
                 <div className="absolute bg-albumOverlay rounded-xl  inset-0 z-[5] "></div>
                 <div className="flex relative flex-col h-full w-full z-20  p-2">
@@ -31,13 +31,13 @@ const Album: React.FC = () => {
                     </div>
                 </div>
             </div>
-            <div className="flex w-full h-[28%] mb-1  justify-between pt-1">
+            <div className="flex w-full h-[28%] mb-1 justify-between pt-1">
                 <Image onClick={() => {
                     dispatch(setCommentModalStatus(true))
-                }} title='Comments' width={100} height={100} className='w-[4rem] h-[3rem]  bg-greyBg  p-2 rounded-xl ' src={CommentIcon} alt='Comment Icon' />
+                }} title='Comments' width={100} height={100} className='w-[4rem] h-[3rem] bl-3d-effect hover:brightness-105 duration-300 transition-all bg-greyBg  p-2 rounded-xl ' src={CommentIcon} alt='Comment Icon' />
                 <Image onClick={() => {
                     dispatch(setUploadPhotoModalStatus(true))
-                }} title='Delete Album' aria-label='Delete Album' width={100} height={100} className='w-[4rem] h-[3rem]  bg-greyBg  p-2 rounded-xl  ' src={DeleteIcon} alt='Comment Icon' />
+                }} title='Delete Album' aria-label='Delete Album' width={100} height={100} className='w-[4rem] h-[3rem] bl-3d-effect hover:brightness-105 duration-300 transition-all  bg-greyBg  p-2 rounded-xl  ' src={DeleteIcon} alt='Comment Icon' />
             </div>
         </div>
     )
