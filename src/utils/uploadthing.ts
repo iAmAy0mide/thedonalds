e008b16b5d7b8fa01d2ae3b2006df56f5234dfe5
iAmAyomide <ayomidekenny952@gmail.com>
2024-08-08 09:00:48 -0700

    added formated date fn

--- src/utils/uploadthing.ts
+++ src/utils/uploadthing.ts
@@ -2,3 +2,12 @@ import { generateUploadDropzone } from "@uploadthing/react";
 import type { OurFileRouter } from "@/app/api/uploadthing/core";
 
 export const Uploader  = generateUploadDropzone<OurFileRouter>();
+
+export const formattedDate = (IsoDate: string) => {
+    const date = new Date(IsoDate);
+    const day = date.getDate();
+    const month = date.toLocaleString("default", { month: "long" });
+    const year = date.getFullYear();
+
+    return `${day} ${month} ${year}`;
+}
