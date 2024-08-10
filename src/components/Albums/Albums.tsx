728f716696f316181dcd69855e320d25133943c7
iAmAyomide <ayomidekenny952@gmail.com>
2024-08-10 14:51:38 -0700

    ups

--- src/components/Albums/Albums.tsx
+++ src/components/Albums/Albums.tsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Album from './Album';
 import Human from "/public/human.svg";
 import { formattedDate } from '@/utils/uploadthing';
 
 const Albums: React.FC<IAlbumsProps> = ({ albums }) => {
+  const [aAlbum, setAalbum] = useState<IAlbumData[]>(albums)
+
+  console.log(albums, "from alubum")
+
+  useEffect(() => {
+
+    (async function fert() {
+      const res = await fetch("http://localhost:3000/api/album", { cache: "no-cache" }) 
+      const album = await res.json();
+      console.log(album, "from update");
+      
+      setAalbum(album);
+    })
+    console.log("albums updated...")
+  }, [aAlbum])
 
 
   return (
     <>
       {
-        albums.map((album) => (
+        aAlbum.map((album) => (
             <Album 
                 key={album._id} 
                 albumId={album._id}
