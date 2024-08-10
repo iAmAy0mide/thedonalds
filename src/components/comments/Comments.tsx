42c837209cdc3b0bcf1ba3f802266c0501cb95b8
iAmAyomide <ayomidekenny952@gmail.com>
2024-08-10 14:38:11 -0700

    using mfucntion

--- src/components/comments/Comments.tsx
+++ src/components/comments/Comments.tsx
@@ -1,18 +1,32 @@
 "use client";
 
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import Spinner from "/public/spinner.gif";
 
 import CommentForm from "./CommentForm";
 
 import Human from "/public/human.svg";
 
-const Comments: React.FC<ICom> = ({ comments }) => {
-  const [famComments, setFamComments] = useState<{
-    _id: string;
-    comment: string;
-  }[]>(comments);
+
+interface Icom {
+  comment: string;
+  _id: string;
+}
+
+
+const Comments: React.FC<IComments> = ({ comments }) => {
+  const [famComments, setFamComments] = useState<Icom[]>(comments || []);
   // console.log(famComments, "from famComments");
+  // console.log(comments, "from famComments - comments");
+
+  useEffect(() => {
+    if (Array.isArray(comments)) {
+      console.log("comments change 2")
+      setFamComments(comments);
+    }
+    console.log("comments change 1")
+  }, [comments]);
   
 
   return (
@@ -20,7 +34,13 @@ const Comments: React.FC<ICom> = ({ comments }) => {
       <div className="bg-actionBg rounded-xl h-[50%] p-1 sm:max-w-[35rem] space-y-2 flex flex-col items-center  md:max-w-[38rem] overflow-auto  w-full mt-2">
 
         {
-          famComments.map((c) => (
+
+          !famComments ? (
+            <div className="mx-auto w-[3.4rem]">
+              <Image width={100} height={100} alt="Loading spinner" src={Spinner} />
+            </div>
+           ) :
+          famComments?.map((c: { _id: React.Key; comment: string; }) => (
           <div key={c._id} className="rounded-lg relative self-start  mx-auto w-full sm:w-[70%] p-1 bg-deepBlue">
             <div className=" h-[4rem] w-[4rem] rounded-lg float-left mr-2">
             <Image width={100} height={100} src={Human} alt='' aria-label='' title='Photo Album Display' className='w-full rounded-lg float-left' />
@@ -37,6 +57,7 @@ const Comments: React.FC<ICom> = ({ comments }) => {
 
 export default Comments;
 
+export const dynamic = "force-dynamic";
 
 interface ICom {
   comments: {
--- src/components/comments/CommentsFetch.tsx
+++ src/components/comments/CommentsFetch.tsx
@@ -1,12 +1,62 @@
+"use client";
+
+import { useSelector } from "react-redux";
+import { updateCurrentAlbumCommentId } from "@/lib/features/store/commentId/commentId";
+
 import { getComments } from "@/app/action";
 import Comments from "./Comments";
+import { RootState } from "@/lib/features/store";
+import { useGetCommentsByIdMMutation, useGetCommentsByIdQuery } from "@/lib/features/api/commentsApiSlice";
+import { useEffect } from "react";
+import { useAlbumContext } from "@/hooks/AlbumContext";
 
-const CommentsFetch = async () => {
+interface IT {
+  currentAlbumId: string
+}
+// interface IComments {
+//   comments: {
+//     _id: string;
+//     comment: string;
+//   }[]
+// }
+
+const CommentsFetch: React.FC= () => {
     // const comments = await getComments();
+    // const currentAlbumId: string = useSelector((state: RootState) => (state.comment.commentId));
+    const { currentAlbumId } = useAlbumContext();
+
+    // console.log({currentAlbumId}, "from cfetch")
+    const { data } = useGetCommentsByIdQuery(currentAlbumId);
+    const comments = data as unknown as  {
+      _id: string;
+      comment: string;
+  }[] 
+    // const comments = com?.comments
+    // console.log(comments, "fr"); 
+    
+    // const [getCommentsById, { data: comments, }] = useGetCommentsByIdMutation();
+
+    // let comments: { _id: string; comment: string; }[] = []
+    // async function getComByID() {
+    //   const res = await fetch(`/api/comments/commentId?commentId=${currentAlbumId}`, { cache: "no-cache"});
+    //   comments = await res.json();
+    // }
+      // await fetch(`/api/comments/commentId/${currentAlbumId}`, { cache: "no-cache"});
+   
+
+
+    
+      // useEffect(() => {
+      //   refetch();
+      //   console.log("refetched comments")
+
+      // }, [currentAlbumId, comments]);
+
+
     const URL = process.env.environment === "prod" ? 'https://thedonalds.vercel.app' : "http://localhost:3000";    
 
-    const res = await fetch(`${URL}/api/comments`, { cache: "no-cache"});
-    const comments = await res.json();
+    // const res = await fetch(`${URL}/api/comments/commentId/${currentAlbumId}`, { cache: "no-cache"});
+    // const comments = await res.json();
     // console.log(comments, "from commentPage");  
     
 
@@ -18,3 +68,10 @@ const CommentsFetch = async () => {
 }
 
 export default CommentsFetch;
+
+export const dynamic = "force-dynamic"
+
+
+interface Id {
+  albumId: string;
+}
