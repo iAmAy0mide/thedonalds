b2b3e0aa7424158843b8c4db2a22478655e64f6c
iAmAyomide <ayomidekenny952@gmail.com>
2024-08-12 07:18:40 -0700

    createdd

---
+++ src/app/api/album/temp-delete/route.ts
@@ -0,0 +1,21 @@
+import dbConnect from "@/lib/db/dbConnect";
+import Album from "@/lib/models/album";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PATCH(req: NextRequest ) {
+    const albumId = req.nextUrl.searchParams.get("albumId");
+    console.log(albumId, "from server here and right now");
+
+    try {
+        await dbConnect();
+
+        const movedAlbum = await Album.findByIdAndUpdate({ _id: albumId }, {
+            deleted: true
+        });
+
+        console.log({movedAlbum}, "from server")
+        return NextResponse.json(movedAlbum);
+    } catch (error: any) {
+        return NextResponse.json({ error: error.message });
+    }
+}
