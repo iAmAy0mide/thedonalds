f8de5f7004129e0143e34e75d147a44b268afee3
iAmAyomide <ayomidekenny952@gmail.com>
2024-08-12 07:24:14 -0700

    m updates

--- src/lib/features/api/apiSlice.ts
+++ src/lib/features/api/apiSlice.ts
@@ -1,9 +1,14 @@
+import { BASE_URL } from "@/utils/constants";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseQuery = fetchBaseQuery({ baseUrl: "http://localhost:3000"})
+// const BASE_URL = () => {
+//     return process.env.environment === "dev" ? "http://localhost:3000" : "https://thedonalds.vercel.app";
+// }
+
+const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL()})
 
 export const apiSlice = createApi({
     baseQuery,
-    // tagTypes: ["Product", "Order", "User", "Category"],
+    tagTypes: ['Album'],
     endpoints: () => ({}),
 });
