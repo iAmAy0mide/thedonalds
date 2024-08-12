8c8e40ff22588a7986a63c0e69a399c07ee76b6e
iAmAyomide <ayomidekenny952@gmail.com>
2024-08-12 07:24:36 -0700

    m updates

--- src/components/Gallery/GalleryPage.tsx
+++ src/components/Gallery/GalleryPage.tsx
@@ -1,17 +1,67 @@
 "use client";
 
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
 import Albums from '../Albums/Albums';
 import { RootState } from '@/lib/features/store';
 import UploadAlbumPhotosModal from "@/components/uploadAlbumPhoto/UploadAlbumPhotosModal";
+import GalleryHomeBtns from '../Albums/GalleryHomeBtns';
 
 
-const GalleryPage = (props: { album: any; children: React.ReactNode; }) => {
+const GalleryPage = (props: { albums: IAlbumData[]; children: React.ReactNode; }) => {
+  const [albums, setAlbums] = useState<IAlbumData[]>(props.albums);
+  const newAlbum = useSelector((state: RootState) => state.newAlbum.newAlbum);
   const isCommentModalOpen = useSelector((state: RootState) => (state.modal.isCommentModalOpen));
   const isUploadAlbumPhotoModalOpen = useSelector((state: RootState) => (state.modal.isUploadPhotoModalOpen));
+
+
+
+  const [shouldRender, setShouldRender] = useState(false);
+
+  console.log(newAlbum, 'from GPage');
+
+  // useEffect(() => {
+  //   let newA = newAlbum;
+  //   if (newA.length > 0) {
+  //         // setAlbums(newAlbum);
+  //         setAlbums((prevAlbums) => [...prevAlbums, ...newAlbum]);
+  //         console.log("rasnsncn here");
+          
+
+  //       newA = [];
+  //     }
+  // });
+
+  // useEffect(() => {
+  //   // if (newAlbum.length > 0) {
+  //     setAlbums(newAlbum);
+  //     // setAlbums((prevAlbums) => [...prevAlbums, ...newAlbum]);
+  //     console.log("rasnsncn here");
+      
+  //     setShouldRender(true);
+  //   // }
+  // }, [newAlbum]);
+
+  // useEffect(() => {
+  //   // if (shouldRender) {
+  //   //   setShouldRender(false);
+  //   // }
+  // }, [shouldRender]);
+
+  // console.log(newAlbum, "from GPage");
+  
+  useEffect(() => {
+    setAlbums(prevAlbums => {
+      const al = [...prevAlbums, ...newAlbum];
+      console.log({al})
+
+      return [...prevAlbums, ...newAlbum]
+    });
+  }, [newAlbum]);
+// console.log(newAlbum, "from GPage effect");
+// console.log("Updated album...")
 
   // const currentAlbumId: string = useSelector((state: RootState) => state.comment.commentId);
   // console.log({currentAlbumId}, "from gall")
@@ -22,14 +72,14 @@ const GalleryPage = (props: { album: any; children: React.ReactNode; }) => {
     ? (document.documentElement.style.overflow = 'hidden')
     : (document.documentElement.style.overflow = 'auto'); 
     
-  }, [ isCommentModalOpen, isUploadAlbumPhotoModalOpen ]);
+  }, [ isCommentModalOpen, isUploadAlbumPhotoModalOpen ]); 
 
 
   return (
     <main className='pt-[12rem] md:pt-[13rem] lg:pt-[14rem] rela tive overflow-hidden -z-20 min-h-screen'>
       `<div className="mobile container mx-auto px-8 md:px-16">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3  gap-2 max-w-[1440px]  w-full justify-center">
-          <Albums albums={props.album} />
+          <Albums albums={albums} renderButtons={GalleryHomeBtns} />
         </div>
       </div>     
       <div className={`${!isCommentModalOpen && "hidden"} absolute left-0 top-[10dvh] z-[600]`}>  
@@ -44,3 +94,6 @@ const GalleryPage = (props: { album: any; children: React.ReactNode; }) => {
 
 
 export default GalleryPage;
+
+
+export const dynamic = "force-dynamic";
