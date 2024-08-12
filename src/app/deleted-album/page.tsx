41b79f4486e9df55140a2f90b4ce0c75eeda4ead
iAmAyomide <ayomidekenny952@gmail.com>
2024-08-12 07:20:15 -0700

    m updates

--- src/app/deleted-album/page.tsx
+++ src/app/deleted-album/page.tsx
@@ -1,12 +1,33 @@
-import Albums from '@/components/Albums/Albums';
-import React from 'react'
+// "use client";
 
-const DeletedGallery = () => {
+import Albums from '@/components/Albums/Albums';
+import DeletedGalleryPageBtn from '@/components/Albums/DeletedGalleryPageBtn';
+import { useGetDeletedAlbumQuery } from '@/lib/features/api/albumApiSlice';
+// import React, { useEffect } from 'react';
+
+
+const DeletedGallery = async () => {
+    // const { data: deletedAlbums, refetch } = useGetDeletedAlbumQuery(undefined, { refetchOnMountOrArgChange: true }); 
+    const res = await fetch("http://localhost:3000/api/album/deleted-album", { cache: "no-cache" });
+    const deletedAlbums = await res.json();
+    // console.log(deletedAlbums, "from deleted")
+
+    // useEffect(() => {
+    //   async function fetg() {
+    //     const res = await fetch("/api/album/deleted-album");
+    //     const data = await res.json();
+
+    //     console.log(data, "from page");
+    //   }
+
+    //   fetg()
+    // }, []);
+    
   return (
     <main className='pt-[12rem] md:pt-[13rem] lg:pt-[14rem] rela tive overflow-hidden -z-20 min-h-screen'>
       `<div className="mobile container mx-auto px-8 md:px-16">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3  gap-2 max-w-[1440px]  w-full justify-center">
-          <Albums albums={[]}  />
+          <Albums albums={deletedAlbums} renderButtons={DeletedGalleryPageBtn}  />
         </div>
       </div>  
       
@@ -15,3 +36,5 @@ const DeletedGallery = () => {
 }
 
 export default DeletedGallery;
+
+export const dynamic = "force-dynamic";
