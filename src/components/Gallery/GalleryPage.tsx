dfda0da9cf958c81e733437af878f96267f05df0
iAmAyomide <ayomidekenny952@gmail.com>
2024-08-12 14:50:18 -0700

    updates

--- src/components/Gallery/GalleryPage.tsx
+++ src/components/Gallery/GalleryPage.tsx
@@ -2,69 +2,29 @@
 
 
 import { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Albums from '../Albums/Albums';
 import { RootState } from '@/lib/features/store';
 import UploadAlbumPhotosModal from "@/components/uploadAlbumPhoto/UploadAlbumPhotosModal";
 import GalleryHomeBtns from '../Albums/GalleryHomeBtns';
+import { updateAlbumPage } from '@/lib/features/store/newAlbum/newAlbum';
 
 
 const GalleryPage = (props: { albums: IAlbumData[]; children: React.ReactNode; }) => {
   const [albums, setAlbums] = useState<IAlbumData[]>(props.albums);
-  const newAlbum = useSelector((state: RootState) => state.newAlbum.newAlbum);
+  const updatedAlbums = useSelector((state: RootState) => state.updatedAlbums.updatedAlbums);
   const isCommentModalOpen = useSelector((state: RootState) => (state.modal.isCommentModalOpen));
   const isUploadAlbumPhotoModalOpen = useSelector((state: RootState) => (state.modal.isUploadPhotoModalOpen));
+  const dispatch = useDispatch();
 
+  console.log(updatedAlbums, 'from GPage');
 
+  // useEffect(()=> {
+  //   console.log("update worked!")
+  // }, [randomString]);
+ 
 
-  const [shouldRender, setShouldRender] = useState(false);
-
-  console.log(newAlbum, 'from GPage');
-
-  // useEffect(() => {
-  //   let newA = newAlbum;
-  //   if (newA.length > 0) {
-  //         // setAlbums(newAlbum);
-  //         setAlbums((prevAlbums) => [...prevAlbums, ...newAlbum]);
-  //         console.log("rasnsncn here");
-          
-
-  //       newA = [];
-  //     }
-  // });
-
-  // useEffect(() => {
-  //   // if (newAlbum.length > 0) {
-  //     setAlbums(newAlbum);
-  //     // setAlbums((prevAlbums) => [...prevAlbums, ...newAlbum]);
-  //     console.log("rasnsncn here");
-      
-  //     setShouldRender(true);
-  //   // }
-  // }, [newAlbum]);
-
-  // useEffect(() => {
-  //   // if (shouldRender) {
-  //   //   setShouldRender(false);
-  //   // }
-  // }, [shouldRender]);
-
-  // console.log(newAlbum, "from GPage");
-  
-  useEffect(() => {
-    setAlbums(prevAlbums => {
-      const al = [...prevAlbums, ...newAlbum];
-      console.log({al})
-
-      return [...prevAlbums, ...newAlbum]
-    });
-  }, [newAlbum]);
-// console.log(newAlbum, "from GPage effect");
-// console.log("Updated album...")
-
-  // const currentAlbumId: string = useSelector((state: RootState) => state.comment.commentId);
-  // console.log({currentAlbumId}, "from gall")
 
   useEffect(() => {
 
@@ -74,10 +34,14 @@ const GalleryPage = (props: { albums: IAlbumData[]; children: React.ReactNode; }
     
   }, [ isCommentModalOpen, isUploadAlbumPhotoModalOpen ]); 
 
+  // useEffect(()=> {
+    console.log("update worked!");
+    // dispatch(updateAlbumPage(albums));
+  // });
 
   return (
-    <main className='pt-[12rem] md:pt-[13rem] lg:pt-[14rem] rela tive overflow-hidden -z-20 min-h-screen'>
-      `<div className="mobile container mx-auto px-8 md:px-16">
+    <main className='pt-[12rem] md:pt-[13rem] lg:pt-[14rem]  overflow-hidden -z-20 min-h-screen'>
+      `<div className="mobile container relative mx-auto px-8 md:px-16">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3  gap-2 max-w-[1440px]  w-full justify-center">
           <Albums albums={albums} renderButtons={GalleryHomeBtns} />
         </div>
