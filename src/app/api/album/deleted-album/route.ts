cd62013dd8dd16bbd7652288fd8140e18b26d10f
iAmAyomide <ayomidekenny952@gmail.com>
2024-08-12 07:19:58 -0700

    m updates

--- src/app/api/album/deleted-album/route.ts
+++ src/app/api/album/deleted-album/route.ts
@@ -1,12 +1,17 @@
 import dbConnect from "@/lib/db/dbConnect";
 import Album from "@/lib/models/album";
+import { revalidatePath } from "next/cache";
 import { NextResponse } from "next/server";
 
 export async function GET() {
     try {
         await dbConnect();
 
-        const deletedAlbums = Album.find({ deleted: true }, { __v: 0, updatedAt: 0, comments: 0 });
+        const deletedAlbums = await Album.find({ deleted: true }, { __v: 0, updatedAt: 0, comments: 0 }).sort({ createdAt: -1 });
+
+        console.log({deletedAlbums});
+
+        revalidatePath("/gallery");
 
         return NextResponse.json(deletedAlbums);
     } catch (error: any) {
