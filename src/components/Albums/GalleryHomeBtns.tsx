806f7dcfdd025b7c4e5e8d819b4240b1dd40bb4c
iAmAyomide <ayomidekenny952@gmail.com>
2024-08-13 02:58:30 -0700

    updates

--- src/components/Albums/GalleryHomeBtns.tsx
+++ src/components/Albums/GalleryHomeBtns.tsx
@@ -8,12 +8,14 @@ import { useDispatch } from 'react-redux';
 import { useMoveAlbumToDeletedMutation } from "@/lib/features/api/albumApiSlice";
 import { updateCurrentAlbumCommentId } from '@/lib/features/store/commentId/commentId';
 import { setCommentModalStatus, setUploadPhotoModalStatus } from '@/lib/features/store/modal/modalSlice';
+import { updateAlbumPage, updateAlbumPageFromDeleteBtn, updateShouldUpdateBol } from "@/lib/features/store/newAlbum/newAlbum";
 
 
 const GalleryHomeBtns = (albumId: string) => {
     const dispatch = useDispatch();
     const [ moveAlbumToDeleted ] = useMoveAlbumToDeletedMutation();
-  
+    
+
     return (
       <>
         <div className="flex w-full h-[28%] mb-1 justify-between pt-1">
@@ -23,7 +25,8 @@ const GalleryHomeBtns = (albumId: string) => {
                   }} title='Comments' width={100} height={100} className='w-[4rem] h-[3rem] bl-3d-effect hover:brightness-105 duration-300 transition-all cursor-pointer bg-greyBg  p-2 rounded-xl ' src={CommentIcon} alt='Comment Icon' />
                   <Image onClick={async() => {
                     console.log("Moved to deleted")
-                  
+                    dispatch(updateAlbumPageFromDeleteBtn({ deleteClicked: true, albumId }));
+                    dispatch(updateShouldUpdateBol(false));
                     const result = await moveAlbumToDeleted(albumId).unwrap();
                     console.log(result);
                   }} title='Delete Album' aria-label='Delete Album' width={100} height={100} className='w-[4rem] h-[3rem] bl-3d-effect hover:brightness-105 duration-300 transition-all cursor-pointer bg-greyBg  p-2 rounded-xl danger ' src={DeleteIcon} alt='Comment Icon' />
