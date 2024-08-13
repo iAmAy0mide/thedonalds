ac597199e7ba112a4d7b0d76b6838d7cef840c29
iAmAyomide <ayomidekenny952@gmail.com>
2024-08-13 14:13:48 -0700

    updated

--- src/app/deleted-album/page.tsx
+++ src/app/deleted-album/page.tsx
@@ -15,9 +15,6 @@ import LoadingSpinner from '@/components/UI/LoadingSpinner';
 
 const DeletedGallery =  () => {
     const { data: deletedAlbums, refetch } = useGetDeletedAlbumQuery(undefined, { refetchOnMountOrArgChange: true }); 
-    // const res = await fetch("http://localhost:3000/api/album/deleted-album", { cache: "no-cache" });
-    // const deletedAlbums = await res.json();
-    // console.log(deletedAlbums, "from deleted");
     const updatedDeletedAlbum = useSelector((state: RootState) => state.updatedAlbums.updatedDeletedAlbums);
     const shouldUpdate = useSelector((state: RootState) => state.updatedAlbums.shouldUpdateDeletedPage);
     const dispatch = useDispatch();
@@ -34,7 +31,7 @@ const DeletedGallery =  () => {
       {
         !deletedAlbums ? (
           <LoadingSpinner />
-        ) : !deletedAlbums[0] ? <h1 className='mx-auto text-[2rem] mt-[10rem] text-actionBg'>Currently empty</h1> : (
+        ) : (
           <main className='pt-[12rem] md:pt-[13rem] lg:pt-[14rem] rela tive overflow-hidden -z-20 min-h-screen'>
             .<div className="mobile container mx-auto px-8 md:px-16">
               <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3  gap-2 max-w-[1440px]  w-full justify-center"> 
