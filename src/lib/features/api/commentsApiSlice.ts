9326737b0b6eeb75e307bc1f60f8ad642f0f8f2e
iAmAyomide <ayomidekenny952@gmail.com>
2024-08-13 14:08:26 -0700

    removed comments

--- src/lib/features/api/commentsApiSlice.ts
+++ src/lib/features/api/commentsApiSlice.ts
@@ -1,16 +1,16 @@
 import { apiSlice } from "./apiSlice";
-
+import { BASE_URL } from "@/utils/constants";
 
 export const commentsApiSlice = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
         getCommentsById: builder.query<IComments, string>({
             query: (currentAlbumId) => ({
-                url: `http://localhost:3000/api/comments/commentId?commentId=${currentAlbumId}`,
+                url: `${BASE_URL()}/api/comments/commentId?commentId=${currentAlbumId}`,
             }),
         }),
         getAlbumPhotosById: builder.query({
             query: (currentAlbumId) => ({
-                url: `http://localhost:3000/api/comments/commentId?commentId=${currentAlbumId}`,
+                url: `${BASE_URL()}/api/comments/commentId?commentId=${currentAlbumId}`,
             }),
         }),
 
