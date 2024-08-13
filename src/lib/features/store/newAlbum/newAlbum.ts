2e1f61f1d51dae059cfde3e5baf11d519083c049
iAmAyomide <ayomidekenny952@gmail.com>
2024-08-13 02:58:53 -0700

    added deletpage actions

--- src/lib/features/store/newAlbum/newAlbum.ts
+++ src/lib/features/store/newAlbum/newAlbum.ts
@@ -1,11 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 interface UpdatedAlbumsData {
-    updatedAlbums: IAlbumData[]
+    updatedAlbums: IAlbumData[],
+    shouldUpdate: boolean;
+    updatedDeletedAlbum: IAlbumData[]
 }
 
 const initialState: UpdatedAlbumsData = {
     updatedAlbums: [],
+    shouldUpdate: true,
+    updatedDeletedAlbum: [],
 }
 
 const NewAlbumSlice = createSlice({
@@ -13,10 +17,24 @@ const NewAlbumSlice = createSlice({
     initialState,
     reducers: {
         updateAlbumPage: (state, action) => {
-            state.updatedAlbums = action.payload;
+            state.updatedAlbums = action.payload?.albums;
+            console.log("This didn't clicked delete button instead");       
+        },
+
+        updateShouldUpdateBol: (state, action) => {
+            state.shouldUpdate = action.payload;
+        },
+
+        updateAlbumPageFromDeleteBtn: (state, action) => {
+            // if (action.payload.deleteClicked === true) {
+                const albums = state.updatedAlbums.filter((album: IAlbumData) => album._id !== action.payload.albumId);
+                state.updatedAlbums = albums;
+                console.log(albums, "update store")
+                console.log("clicked delete button")
+            // }
         }
     }
 });
 
-export const { updateAlbumPage } = NewAlbumSlice.actions;
+export const { updateAlbumPage, updateShouldUpdateBol, updateAlbumPageFromDeleteBtn } = NewAlbumSlice.actions;
 export default NewAlbumSlice.reducer;
