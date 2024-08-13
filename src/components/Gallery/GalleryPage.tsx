9c4d72d8609e175854fbf172930f15d518ff0575
iAmAyomide <ayomidekenny952@gmail.com>
2024-08-13 02:59:22 -0700

    ups

--- src/components/Gallery/GalleryPage.tsx
+++ src/components/Gallery/GalleryPage.tsx
@@ -13,12 +13,15 @@ import { updateAlbumPage } from '@/lib/features/store/newAlbum/newAlbum';
 
 const GalleryPage = (props: { albums: IAlbumData[]; children: React.ReactNode; }) => {
   const [albums, setAlbums] = useState<IAlbumData[]>(props.albums);
+
   const updatedAlbums = useSelector((state: RootState) => state.updatedAlbums.updatedAlbums);
+
   const isCommentModalOpen = useSelector((state: RootState) => (state.modal.isCommentModalOpen));
   const isUploadAlbumPhotoModalOpen = useSelector((state: RootState) => (state.modal.isUploadPhotoModalOpen));
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
-  console.log(updatedAlbums, 'from GPage');
+  // console.log(updatedAlbums, 'from GPage');
+
 
   // useEffect(()=> {
   //   console.log("update worked!")
@@ -35,15 +38,24 @@ const GalleryPage = (props: { albums: IAlbumData[]; children: React.ReactNode; }
   }, [ isCommentModalOpen, isUploadAlbumPhotoModalOpen ]); 
 
   // useEffect(()=> {
-    console.log("update worked!");
-    // dispatch(updateAlbumPage(albums));
-  // });
+  //   console.log("update worked!");
+  //   console.log(updatedAlbums);
+  //   dispatch(updateAlbumPage(albums));
+  // }, [albums, updatedAlbums, dispatch]);
+
+  // if (albums && albums.length > 0) {
+  //   dispatch(updateAlbumPage(albums));
+  // }
+
+  // useEffect(() => {
+  //   dispatch(updateAlbumPage(albums));
+  // }, [dispatch, albums]);
 
   return (
     <main className='pt-[12rem] md:pt-[13rem] lg:pt-[14rem]  overflow-hidden -z-20 min-h-screen'>
       `<div className="mobile container relative mx-auto px-8 md:px-16">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3  gap-2 max-w-[1440px]  w-full justify-center">
-          <Albums albums={albums} renderButtons={GalleryHomeBtns} />
+          <Albums albums={updatedAlbums} renderButtons={GalleryHomeBtns} />
         </div>
       </div>     
       <div className={`${!isCommentModalOpen && "hidden"} absolute left-0 top-[10dvh] z-[600]`}>  
