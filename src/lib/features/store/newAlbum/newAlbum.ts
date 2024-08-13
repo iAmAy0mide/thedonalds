983bbe6aafe8c40966920350df2905dbd56ac6a9
iAmAyomide <ayomidekenny952@gmail.com>
2024-08-13 07:01:44 -0700

    updates

--- src/lib/features/store/newAlbum/newAlbum.ts
+++ src/lib/features/store/newAlbum/newAlbum.ts
@@ -3,15 +3,15 @@ import { createSlice } from "@reduxjs/toolkit";
 interface UpdatedAlbumsData {
     updatedAlbums: IAlbumData[],
     shouldUpdate: boolean;
-    updatedDeletedAlbum: IAlbumData[],
-    shouldUpdateDeleted: boolean;
+    updatedDeletedAlbums: IAlbumData[],
+    shouldUpdateDeletedPage: boolean;
 }
 
 const initialState: UpdatedAlbumsData = {
     updatedAlbums: [],
     shouldUpdate: true,
-    updatedDeletedAlbum: [],
-    shouldUpdateDeleted: true,
+    updatedDeletedAlbums: [],
+    shouldUpdateDeletedPage: true,
 }
 
 const NewAlbumSlice = createSlice({
@@ -37,9 +37,25 @@ const NewAlbumSlice = createSlice({
             // if (action.payload.deleteClicked === true) {
                 const albums = state.updatedAlbums.filter((album: IAlbumData) => album._id !== action.payload.albumId);
                 state.updatedAlbums = albums;
-                console.log(albums, "update store")
-                console.log("clicked delete button")
+                console.log(albums, "update store");
+                console.log("clicked delete button");
             // }
+        },
+
+        updateDeletedAlbums: (state, action) => {
+            state.updatedDeletedAlbums = action.payload;
+        },
+
+        updateDeletedAlbumsFromBtn: (state, action) => {
+            const albums = state.updatedDeletedAlbums.filter((album: IAlbumData) => album._id !== action.payload );
+                state.updatedDeletedAlbums = albums;
+                console.log(albums, "update del store");
+                console.log("clicked restore or perm button");
+            // state.updatedDeletedAlbums = state.updatedDeletedAlbums.filter((album: IAlbumData) => album._id !== action.payload.albumId);
+        },
+
+        updateShouldUpdateDeletedPageBol: (state, action) => {
+            state.shouldUpdateDeletedPage = action.payload;
         }
     }
 });
@@ -48,5 +64,8 @@ export const {
     updateAlbumPage, 
     updateShouldUpdateBol, 
     updateAlbumPageFromForm, 
-    updateAlbumPageFromDeleteBtn } = NewAlbumSlice.actions;
+    updateAlbumPageFromDeleteBtn,
+    updateDeletedAlbums,
+    updateDeletedAlbumsFromBtn,
+    updateShouldUpdateDeletedPageBol, } = NewAlbumSlice.actions;
 export default NewAlbumSlice.reducer;
