1c6f5f8ff843712488ad13b3acde13f6c9d5dbfb
iAmAyomide <ayomidekenny952@gmail.com>
2024-08-14 09:46:16 -0700

    update design

--- src/components/Albums/DeletedGalleryPageBtn.tsx
+++ src/components/Albums/DeletedGalleryPageBtn.tsx
@@ -2,8 +2,9 @@
 
 import React from 'react'
 import Image from "next/image";
-import PermDel from "/public/perm-delete.svg";
-import Restore from "/public/restore-illust.svg";
+import Recycle from "/public/recycle_icon.svg";
+import DeleteIcon from "/public/DeleteIcon.svg";
+import Caution3 from "/public/caution_2.svg";
 
 import { useDeleteAlbumMutation, useRestoreAlbumMutation } from '@/lib/features/api/albumApiSlice';
 import { useDispatch } from 'react-redux';
@@ -19,36 +20,42 @@ const DeletedGalleryPageBtn = (albumId: string) => {
       <div className="flex w-full h-[28%] bg -red mb-1 justify-between pt-1">
         <div 
           onClick={() => {
-            // console.log("restored", albumId);
             dispatch(updateShouldUpdateDeletedPageBol(false));
             dispatch(updateDeletedAlbumsFromBtn(albumId));
             restoreAlbum(albumId);
           }} 
 
-          className=" cursor-pointer rounded-xl flex items-center justify-center">
+          className="cursor-pointer flex bl-3d-effect hover:brightness-105 duration-300 transition-all rounded-xl danger2 w-[4rem] h-[3rem]  bg-greyBg items-center relative justify-center p-2">
           <Image 
             title='Restore Album' 
             aria-label='Restore Album' 
             width={100} 
             height={100} 
-            className='w-[4rem] h-[3rem] bl-3d-effect hover:brightness-105 duration-300 transition-all cursor-pointer bg-greyBg rounded-xl ' src={Restore} alt='Comment Icon' />
+            className='w-full rounded-xl ' 
+            src={Recycle} alt='Comment Icon' />
         </div>
 
         <div                    
           onClick={async () => {
-            // console.log("deleted", albumId);
             dispatch(updateShouldUpdateDeletedPageBol(false));
             dispatch(updateDeletedAlbumsFromBtn(albumId))
             const deletedAlbum = await deleteAlbum(albumId).unwrap();
-            // console.log(deletedAlbum, "from del btn")
           }}
-          className="cursor-pointer flex items-center justify-center">
+          className="cursor-pointer flex bl-3d-effect hover:brightness-105 duration-300 transition-all rounded-xl danger2 w-[4rem] h-[3rem] bg-greyBg items-center relative justify-center p-2">
           <Image 
             title='Delete Album Permanently' 
             aria-label='Delete Album Permanently' 
             width={100} 
             height={100} 
-            className='w-[4rem] h-[3rem] bl-3d-effect hover:brightness-105 duration-300 transition-all cursor-pointer bg-greyBg rounded-xl danger2 ' src={PermDel} alt='Comment Icon' />
+            className='w-[2rem] h-[2rem] ' src={DeleteIcon} alt='Comment icon' />
+            <div className="absolute w-[1.2rem] h-[1.2rem] top-1 right-2 ">
+              <Image 
+                title='Delete Album Permanently' 
+                aria-label='Delete Album Permanently' 
+                width={100} 
+                height={100} 
+                className='w-full' src={Caution3} alt='Caution or danger icon' />
+            </div>
         </div>
       </div>
     </>
