d463274d65208d0c1ec4eebac135684d83930241
iAmAyomide <ayomidekenny952@gmail.com>
2024-08-15 09:10:24 -0700

    updates

--- src/components/comments/CommentsModalCloseBtn.tsx
+++ src/components/comments/CommentsModalCloseBtn.tsx
@@ -2,6 +2,9 @@
 import { setCommentModalStatus } from '@/lib/features/store/modal/modalSlice';
 import React from 'react'
 import { useDispatch } from 'react-redux';
+import CloseIcon from "/public/close_icon.svg";
+import Close from "/public/close.svg";
+import Image from "next/image";
 
 const CommentsModalCloseBtn = () => {
     const dispatch = useDispatch();
@@ -9,7 +12,15 @@ const CommentsModalCloseBtn = () => {
   return (
     <button onClick={() => {
         dispatch(setCommentModalStatus(false));
-      }} title='Close' aria-label='Close comment modal button' className="close-modal-button">Close</button>
+      }} title='Close' aria-label='Close comment modal button' className=" close-modal-button">
+        <Image
+          width={100}
+          height={100}
+          src={Close}
+          alt="Icon to close modal"
+          className='w-full rounded-full  bg-actionBg  p-1'
+        />
+      </button>
   )
 }
 
