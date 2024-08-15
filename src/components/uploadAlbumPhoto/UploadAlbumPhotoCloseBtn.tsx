6f5a4422c29b987906d9a572969e8c828625b64d
iAmAyomide <ayomidekenny952@gmail.com>
2024-08-15 09:11:44 -0700

    ups

--- src/components/uploadAlbumPhoto/UploadAlbumPhotoCloseBtn.tsx
+++ src/components/uploadAlbumPhoto/UploadAlbumPhotoCloseBtn.tsx
@@ -2,14 +2,24 @@
 
 import { setUploadPhotoModalStatus } from "@/lib/features/store/modal/modalSlice";
 import { useDispatch } from "react-redux";
+import Close from "/public/close.svg";
+import Image from "next/image";
 
 const UploadAlbumPhotoCloseBtn = () => {
     const dispatch = useDispatch();
 
   return (
     <button onClick={() => {
-        dispatch(setUploadPhotoModalStatus(false));
-      }} title='Close' aria-label='Close upload modal button' className="close-modal-button">Close</button>
+      dispatch(setUploadPhotoModalStatus(false));
+    }} title='Close' aria-label='Close comment modal button' className=" close-modal-button">
+      <Image
+        width={100}
+        height={100}
+        src={Close}
+        alt="Icon to close modal"
+        className='w-full rounded-full  bg-actionBg  p-1'
+      />
+    </button>
   )
 }
 
