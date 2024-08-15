e2a7f3f3b46bb5b98668487b9ed617788ae9143b
iAmAyomide <ayomidekenny952@gmail.com>
2024-08-15 01:18:34 -0700

    added albumcarousel

--- src/components/comments/CommentsModal.tsx
+++ src/components/comments/CommentsModal.tsx
@@ -22,8 +22,8 @@ const CommentsModal = () => {
       <CommentsModalCloseBtn /> 
       <AlbumContextProvider currentAlbumId={currentAlbumId}>
         <div className=" relative z-20 bg-greenBg   h-[85dvh] border-[4px] border-red rounded-xl px-4">
-          <div className="h-full mt- w-full  justify-center items-center gap -2 rounded-xl flex flex-col mx-auto">
-              {/* {children} */}
+          <div className="h-full mt- w-full py-1  justify-center items-center gap -2 rounded-xl flex flex-col mx-auto">
+              <AlbumPhotoCarousel />
               <CommentSection />
           </div>
         </div>  
