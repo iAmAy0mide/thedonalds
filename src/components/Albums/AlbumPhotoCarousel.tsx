6a37a0b1c5c928a5a76c279961d7334705b8fe36
iAmAyomide <ayomidekenny952@gmail.com>
2024-08-16 04:14:37 -0700

    ups

--- src/components/Albums/AlbumPhotoCarousel.tsx
+++ src/components/Albums/AlbumPhotoCarousel.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { useEffect, useState } from "react"
 
 import { Swiper, SwiperSlide}  from "swiper/react";
 
@@ -11,39 +12,48 @@ import { EffectCoverflow, Pagination, Navigation } from "swiper/modules";
 import Human from "/public/human.svg";
 
 const AlbumPhotoCarousel = () => {
+    const [slidesPerView, setSlidePerview] = useState<number>(3);
+
+    useEffect(() => {
+      const sw = window.document
+    }, []);
+
+
   return (
     <div className="mx-auto  bg-red rounded-xl flex justify-center  album-resp-mobile  md: px- 16 max-w-[30rem] max-sm:max-w-[2 5rem] w-full h-[10rem]  p-1">
       <Swiper
         effect={'coverflow'}
         grabCursor={true}
         centeredSlides={false}
-        // slidesPerView={3}
         slidesPerView={2}
+        // slidesPerView={2}
         // slidesPerView={"auto"}
         coverflowEffect={{
           rotate: 90,
-          stretch: 0,
-          depth: 100,
-          modifier: 2,
+          stretch: 2,
+          // stretch: 0,
+          depth: 10,
+          // depth: 100,
+          modifier: 20,
           slideShadows: true,
         }}
         // pagination={true}
         modules={[EffectCoverflow, Pagination]}
-        className="mySw iper w-full bg-deepBlue" 
+        className="mySw iper w-full bg-deep Blue" 
       >
-        <SwiperSlide className="max-sm:w-[1 6rem] max-sm:h-full s-sl ide  bg-actionBg">
+        <SwiperSlide className="bg-actionBg">
             <Image width={100} height={100} alt={""} src={Human} className="w-full max-sm:h-full" />
         </SwiperSlide>
-        <SwiperSlide className="max-sm:w-[1 6rem] max-sm:h- s-sl ide  bg-actionBg">
+        <SwiperSlide className="bg-actionBg">
             <Image width={100} height={100} alt={""} src={Human} className="w-full " />
         </SwiperSlide>
-        <SwiperSlide className="max-sm:w-[1 6rem] max-sm :h-full s-sl ide  bg-actionBg">
+        <SwiperSlide className=" bg-actionBg">
             <Image width={100} height={100} alt={""} src={Human} className="w-full " />
         </SwiperSlide>
-        <SwiperSlide className="max-sm:w-[1 6rem] max-sm :h-full s-sl ide  bg-actionBg">
+        <SwiperSlide className="bg-actionBg">
             <Image width={100} height={100} alt={""} src={Human} className="w-full  " />
         </SwiperSlide>
-        <SwiperSlide className="max-sm:w-[1 6rem] max-sm :h-full s-sl ide  bg-actionBg">
+        <SwiperSlide className=" bg-actionBg">
             <Image width={100} height={100} alt={""} src={Human} className="w-full " />
         </SwiperSlide>
 
