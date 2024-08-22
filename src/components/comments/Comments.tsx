9c2c733025d9a269c4c8641994a149153d056d94
iAmAyomide <ayomidekenny952@gmail.com>
2024-08-22 03:33:07 -0700

    ups

--- src/components/comments/Comments.tsx
+++ src/components/comments/Comments.tsx
@@ -31,9 +31,8 @@ const Comments: React.FC<IComments> = ({ comments }) => {
     <>
       <div className="bg-actionBg rounded-xl h-[50%] p-1 sm:max-w-[35rem] space-y-2 flex flex-col items-center  md:max-w-[38rem] overflow-auto  w-full mt-2">
 
-        {
-
-          !famComments ? (
+      {
+        !famComments ? (
           <div className="mx-auto w-[2rem]">
             <Image
               src={Spinner}
@@ -43,22 +42,28 @@ const Comments: React.FC<IComments> = ({ comments }) => {
               className="w-full"
               unoptimized
             />
-          </div>)  : (
-
-          Array.isArray(famComments) &&
-          
-          famComments[0] === undefined 
-            ? (<p className="text-[1.5rem] text-center">Whoops! No comments.</p>) 
-            : famComments.map((c: { _id: React.Key; comment: string; }) => (
-                <div key={c._id} className="rounded-lg relative self-start  mx-auto w-full sm:w-[70%] p-1 bg-deepBlue">
-                  <div className=" h-[4rem] w-[4rem] rounded-lg float-left mr-2">
-                  <Image width={100} height={100} src={Human} alt='' aria-label='' title='Photo Album Display' className='w-full rounded-lg float-left' />
-                  </div>
-                  <p className="comment-text overflow-clip">{c.comment}</p>
-                </div>
-              ))
-          )
-        }
+          </div>
+        ) : Array.isArray(famComments) && famComments.length === 0 ? (
+          <p className="text-[1.5rem] text-center">Whoops! No comments.</p>
+        ) : Array.isArray(famComments) ? (
+          famComments.map((c: { _id: React.Key; comment: string }) => (
+            <div key={c._id} className="rounded-lg relative self-start mx-auto w-full sm:w-[70%] p-1 bg-deepBlue">
+              <div className="h-[4rem] w-[4rem] rounded-lg float-left mr-2">
+                <Image
+                  width={100}
+                  height={100}
+                  src={Human}
+                  alt="" 
+                  className="w-full rounded-lg float-left"
+                />
+              </div>
+              <p className="comment-text overflow-clip">{c.comment}</p>
+            </div>
+          ))
+        ) : (
+          <p className="text-[1.5rem] text-center">Unexpected data format.</p>
+        )
+      }
       </div>
       <CommentForm setFamComments={setFamComments} />
     </>
