aedb5f91780824e064759089fa1832d84222854d
iAmAyomide <ayomidekenny952@gmail.com>
2024-08-24 03:29:26 -0700

    ups

--- src/components/Login/LoginForm.tsx
+++ src/components/Login/LoginForm.tsx
@@ -1,57 +1,50 @@
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { useDispatch, useSelector } from 'react-redux';
-import { setLoginModalStatus } from '@/lib/features/store/auth/authSlice';
-import { signIn } from 'next-auth/react';
+
 import Image from "next/image";
-import Star from "/public/Star.svg";
-import GoogleLogo from "/public/google-logo.webp";
+
+import { useRouter } from 'next/navigation';
+import { useDispatch } from 'react-redux';
+import { signIn } from 'next-auth/react';
+
+import { setLoginModalStatus } from '@/lib/features/store/auth/authSlice';
 import GoogleLogo2 from "/public/logo_google.svg";
+import Close from "/public/close.svg";
 
     
 const LoginForm = () => {
-    const [password, setPassword] = useState<string>("");
+  
     const router = useRouter();
     const dispatch = useDispatch();
 
   return (
-    <div className='glassy-white-sm-menu glassy-whit e-sm-menu backdrop-brightness-110 border-[2px] relative border-[#ccc] max -w-[25rem] min-w-[20rem] sm:min-w-[25rem] rounded-xl p-4 flex flex-col'>
-   
-      <div className="flex w-full flex-col justify-center items-center ">
-        {/* <div 
-          onClick={() => {
-              dispatch(setLoginModalStatus(false));
-              signIn("google")
-            
-          }} 
-          className='px-8 py-3 flex mb-4 action text-[#fff] rounded-xl shadow-inner '>
-           <div className="w-[2rem]">
-            <Image
-              width={100}
-              height={100}
-              alt=''
-              src={Star}
-              className=''
-            />
-          </div> <p>Google</p> 
-        </div> */}
-        <p className='text-[2rem] font-bold mb-8 place-self-start login-h'>Login</p>
+    <div className='glassy-white -sm-menu relative border-[2px] border-[#ccc] min-w-[20rem] sm:min-w-[25rem] rounded-xl p-4 flex flex-col overflow-hidden'>
+      <div className="absolute bg-[#fff] h-[120%] w-4 -rotate-45 left-4"></div>
+      <div className="absolute bg-[#fff] h-[30rem] w-8 -top-20 rotate-50 right-[18%] sm:right-[24%]"></div>
+      <div className="absolute bg-[#fff] h-[30rem] w-1 -top-20 rotate-50 right-[14%] sm:right-[21%]"></div>
+      <button title="Close" onClick={() => dispatch(setLoginModalStatus(false))} className="absolute -right-3 -top-3 bg  pt-4 pb-2 pl-2 pr-4 rounded-full w-[3rem]  brightness-200 hover:brightness-150 transition-all duration-300">
+        <Image 
+          alt=""
+          src={Close}
+          width={100}
+          height={100}
+        />
+      </button>
+      <div className="flex w-full flex-col justify-center items-center pb-6">
+        <p className='text-[2rem] font-bold text-[#ccc] mb-8 place-self-start'>Login</p>
         <div 
-          onClick={() => {
+          onClick={async () => {
               dispatch(setLoginModalStatus(false));
-              signIn("google")
-            
+              signIn('google', { callbackUrl: '/gallery' });            
           }} 
-          className='px-16 py-3 flex cursor-pointer glassy-green-login justify-between items-center w-f text-[#fff] rounded-xl '>
-          <div className="w-[3.3rem]">
+          className='px-10 relative  flex cursor-pointer glassy-green-login justify-between items-center text-[#fff] rounded-xl overflow-hidden mt-4 sm:mt-10'>
+          <div className="absolute bg-[#fff] h-[6rem] w-4 -rotate-45 left-4"></div>
+          <div className="w-[2.5rem]">
             <Image
               width={100}
               height={100}
-              alt=''
+              alt='google logo'
               src={GoogleLogo2}
-              className=''
             />
-          </div> <p className='text-xl login font-extrabold'>Google</p> 
+          </div> <p className='text-2xl -ml-1 login font-extrabold'>oogle</p> 
         </div>
       </div>
       
